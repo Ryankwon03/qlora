bb0e7a280911f6e3d289d3e38424dc9f7da32930
Ryan Sungmo Kwon <ryankwon@umich.edu>
2024-09-17 12:27:47 -0400

    Fix: fix run2.sh

--- run2.sh
+++ run2.sh
@@ -3,4 +3,4 @@
 python3 qlora.py \
 	--model_name_or_path "mistralai/Mistral-7B-Instruct-v0.3" \
 	--dataset_name "allenai/wildjailbreak" \
-    	--dataset_format "hh-rlhf"
+    --dataset_format "hh-rlhf"
